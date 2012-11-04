ceca03dc37babf3c9e27fb2f6cdbee59dc0e7185
Jason Nelson <jason@carbonmade.com>
2012-11-04 17:57:37 -0500

    Tests

---
+++ src/Carbon.Core.Tests/DateTimeExtensionTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Carbon.Helpers.Tests
+{
+	using System;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class DateTimeExtensions
+	{
+		[Test]
+		public void ChangeDatePrecisionTests()
+		{
+			Assert.AreEqual(new DateTime(2011, 01, 01), new DateTime(2011, 02, 11, 15, 12, 11).ChangePrecision(TimeUnit.Year));
+			Assert.AreEqual(new DateTime(2011, 02, 01), new DateTime(2011, 02, 11, 15, 12, 11).ChangePrecision(TimeUnit.Month));
+			Assert.AreEqual(new DateTime(2011, 02, 11), new DateTime(2011, 02, 11, 15, 12, 11).ChangePrecision(TimeUnit.Day));
+			// Assert.AreEqual(new DateTime(2011, 01, 01), new DateTime(2011, 02, 11, 15, 12, 11).ChangePrecision(TimeUnit.Day));
+			// Assert.AreEqual(new DateTime(2011, 01, 01), new DateTime(2011, 02, 11, 15, 12, 11).ChangePrecision(TimeUnit.Day));
+			// Assert.AreEqual(new DateTime(2011, 01, 01), new DateTime(2011, 02, 11, 15, 12, 11).ChangePrecision(TimeUnit.Day));
+		}
+	}
+}
---
+++ src/Carbon.Core.Tests/Validation/ValidationErrorCollectionTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Carbon.Validation.Tests
+{
+	using System;
+	using System.IO;
+	using System.Runtime.Serialization.Formatters.Binary;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class ValidationErrorCollectionTests
+	{
+		[Test]
+		public void SerializeTest()
+		{
+			var errors = new ValidationErrorCollection();
+
+			errors.Add("key", "error 1");
+			errors.Add("error 2");
+
+			var formatter = new BinaryFormatter();
+
+			using (var stream = new MemoryStream())
+			{
+				formatter.Serialize(stream, errors);
+
+				var data = Convert.ToBase64String(stream.ToArray());
+
+				stream.Position = 0;
+
+				var errors2 = (ValidationErrorCollection)formatter.Deserialize(stream);
+
+				Assert.AreEqual(2, errors2.Count);
+				Assert.AreEqual("error 1", errors2[0].Message);
+				Assert.AreEqual("key", errors2[0].Key);
+				Assert.AreEqual("error 2", errors2[1].Message);
+			}
+		}
+	}
+}
