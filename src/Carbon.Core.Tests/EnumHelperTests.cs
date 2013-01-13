84c43580c81ad55494839bebbe058454ce33a6b9
Jason Nelson <jason@carbonmade.com>
2013-01-13 16:41:34 -0600

    Add EnumHelper

---
+++ src/Carbon.Core.Tests/EnumHelperTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Carbon.Helpers.Tests
+{
+	using System;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class EnumHelperTests
+	{
+		[Test]
+		public void One()
+		{
+			var items = EnumHelper.GetItems<int>(typeof(Things));
+
+			Assert.AreEqual("Apple", items[0].Name);
+			Assert.AreEqual(1, items[0].Value);
+
+			Assert.AreEqual(3, items.Count);
+		}
+	}
+
+	public enum Things
+	{
+		Apple = 1,
+		House,
+		Car
+	}
+}
---
+++ src/Carbon.Core/EnumHelper.cs
@@ -0,0 +1,54 @@
+﻿namespace Carbon.Helpers
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	public static class EnumHelper
+	{
+		public static IList<EnumItem<T>> GetItems<T>(Type type)
+		{
+			#region Preconditions
+
+			if (type == null) 
+				throw new ArgumentNullException("type");
+
+			if (!type.IsEnum)
+				throw new ArgumentException("Must be an Enum", paramName: "type");
+
+			#endregion
+
+			var names = Enum.GetNames(type);
+			var values = new List<T>();
+
+			foreach (var o in Enum.GetValues(type))
+			{
+				values.Add((T)o);
+			}
+
+			return names.Zip(values, (name, value) => new EnumItem<T>(name, (T)value)).ToArray();
+		}
+
+		public class EnumItem<T>
+		{
+			private readonly string name;
+			private readonly T value;
+
+			public EnumItem(string name, T value)
+			{
+				this.name = name;
+				this.value = value;
+			}
+
+			public string Name 
+			{
+				get { return name; }
+			}
+
+			public T Value
+			{
+				get { return value; }
+			}
+		}
+	}
+}
